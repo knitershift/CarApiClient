39c96a02d75e11edd4d2033ce2c8dbc63d31bf12
Ярослав Ігорович <yaroslaw@live.ru>
2019-06-28 16:05:59 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CarApiClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ColumnId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Manager";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxName.Location = new System.Drawing.Point(36, 67);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(160, 29);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Price";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxPrice.Location = new System.Drawing.Point(220, 67);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 29);
+            this.textBoxPrice.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(333, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Color";
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxColor.Location = new System.Drawing.Point(336, 67);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(100, 29);
+            this.textBoxColor.TabIndex = 1;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.buttonCreate.Location = new System.Drawing.Point(453, 67);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(96, 29);
+            this.buttonCreate.TabIndex = 2;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnId});
+            this.listView1.Location = new System.Drawing.Point(36, 125);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(513, 326);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 486);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxColor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ColumnId;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using CarApiClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarApiClient
+{
+    public partial class Form1 : Form
+    {
+        public const string API_URL = "http://localhost:49890/api/autos";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var request = WebRequest.Create(API_URL) as HttpWebRequest;
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            var sendStream = request.GetRequestStream();
+
+            var auto = new Auto { Name = "Mercedes", Price = 15555, Color = "Gray" };
+            string json = JsonConvert.SerializeObject(auto);
+
+            byte[]data = Encoding.UTF8.GetBytes(json);
+            sendStream.Write(data, 0, data.Length);
+
+            var response = request.GetResponse() as HttpWebResponse;
+
+            string result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            Auto createdAuto = JsonConvert.DeserializeObject<Auto>(result);
+
+            MessageBox.Show($"Auto id = {createdAuto.Id}");
+        }
+    }
+}
